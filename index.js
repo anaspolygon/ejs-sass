778d2b54cda0d38c756e6ba341ba1cfd43f74c7f
alanasBD <alanas7171@gmail.com>
2023-02-19 21:15:08 +0600

    home route ,recruit pages,company pages created

--- index.js
+++ index.js
@@ -9,12 +9,26 @@ app.set("view engine", "ejs");
 let languages = [];
 
 app.get("/", (req, res) => {
-  res.render("company");
+  res.render("home");
+});
+
+app.get("/home", (req, res) => {
+  res.render("home");
 });
 
 app.get("/company", (req, res) => {
   res.render("company");
 });
+app.get("/company/info", (req, res) => {
+  res.render("company-info");
+});
+app.get("/company/strategy", (req, res) => {
+  res.render("company-strategy");
+});
+app.get("/company/photo", (req, res) => {
+  res.render("company-photo");
+});
+
 app.get("/member", (req, res) => {
   res.render("member");
 });
@@ -27,20 +41,22 @@ app.get("/recruit", (req, res) => {
 });
 
 app.get("/recruit/info", (req, res) => {
-  res.render("recruit/info", { languages: languages });
+  res.render("recruit-info");
 });
-
-
 
 app.get("/recruit/message", (req, res) => {
-  res.render("recruit/message");
+  res.render("recruit-message");
 });
 app.get("/recruit/workstyle", (req, res) => {
-  res.render("recruit/workstyle");
+  res.render("recruit-workstyle");
 });
 app.get("/recruit/faq", (req, res) => {
-  res.render("recruit/faq");
+  res.render("recruit-faq");
 });
+app.get("/crosstalk", (req, res) => {
+  res.render("crosstalk");
+});
+
 
 app.post("/", (req, res) => {
   const lang = req.body.pLanguage;
